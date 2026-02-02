ec8f05a314ea709dbd0efbf884ecd1cd7645d020
wei-guoning <weiguoning2@huawei.com>
2026-02-02 17:49:41 +0800

    add framework code
    Signed-off-by: wei-guoning <weiguoning2@huawei.com>

---
+++ frameworks/kits/extension/include/object_editor_extension_module_loader.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Huawei Device Co., Ltd. 2026-2026. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_OBJECT_EDITOR_OBJECT_EDITOR_EXTENSION_MODULE_LOADER_H
+#define OHOS_OBJECT_EDITOR_OBJECT_EDITOR_EXTENSION_MODULE_LOADER_H
+
+#include "extension_module_loader.h"
+
+namespace OHOS {
+namespace AbilityRuntime {
+
+class ObjectEditorExtensionModuleLoader : public ExtensionModuleLoader,
+                                          public Singleton<ObjectEditorExtensionModuleLoader> {
+    DECLARE_SINGLETON(ObjectEditorExtensionModuleLoader);
+
+public:
+    /**
+     * @brief Create Extension.
+     *
+     * @param runtime The runtime.
+     *
+     * @return The Extension instance.
+     */
+    virtual Extension *Create(const std::unique_ptr<Runtime> &runtime) const override;
+
+    virtual std::map<std::string, std::string> GetParams() override;
+};
+
+} // namespace AbilityRuntime
+} // namespace OHOS
+#endif // OHOS_OBJECT_EDITOR_OBJECT_EDITOR_EXTENSION_MODULE_LOADER_H
