d5e86650e9143405074f12e66058437adc89f7fe
万孝国 <wanxiaoguo@huawei.com>
2026-02-09 15:25:36 +0800

    SA 和 extension 关闭
    Signed-off-by: 万孝国 <wanxiaoguo@huawei.com>

--- system_ability/include/object_editor_connection.h
+++ system_ability/include/object_editor_connection.h
@@ -16,19 +16,21 @@
 #ifndef OHOS_OBJECT_EDITOR_OBJECT_EDITOR_CONNECTION_H
 #define OHOS_OBJECT_EDITOR_OBJECT_EDITOR_CONNECTION_H
 
+#include <thread>
+
 #include "ability_connect_callback_stub.h"
 #include "ability_manager_client.h"
 
 #include "hilog_object_editor.h"
+#include "iobject_editor_connection_status_callback.h"
 #include "object_editor_err_code.h"
 
 namespace OHOS {
 namespace ObjectEditor {
-
 class ObjectEditorConnection : public AAFwk::AbilityConnectCallbackStub {
 public:
     ObjectEditorConnection();
-    virtual ~ObjectEditorConnection() = default;
+    ~ObjectEditorConnection();
 
     void OnAbilityConnectDone(const AppExecFwk::ElementName &element,
         const sptr<IRemoteObject> &remoteObject, int32_t resultCode) override;
@@ -36,6 +38,7 @@ public:
     ObjectEditorManagerErrCode StartConnect(const std::string &bundleName, const std::string &abilityName,
         const std::string &moduleName, sptr<IRemoteObject> &remoteObject);
     ObjectEditorManagerErrCode StopConnect();
+    void RegisterConnectionStatusCallback(const std::shared_ptr<IObjectEditorConnectionStatusCallback> callback);
 
 protected:
     const int8_t CONNECT_TIME_OUT = 3;
@@ -48,6 +51,20 @@ private:
     sptr<IRemoteObject> extensionProxy_ = nullptr;
     std::condition_variable connectCondition_;
     bool isConnectReady_ = false;
+    std::mutex connectionStatusCallbackMutex_;
+    std::shared_ptr<IObjectEditorConnectionStatusCallback> connectionStatusCallback_ = nullptr;
+
+    void TimerThreadStopExtension();
+    void ResetStopExtensionTimer();
+    void CheckRemoteEditStatus(bool *isEditing, bool *isModified);
+
+    std::mutex mutexTimer_;
+    std::mutex mutexTimerRunning_;
+    std::condition_variable cvTimer_;
+    std::atomic<bool> timerRunning_{false};
+    std::atomic<bool> timerNotify__{false};
+    std::thread timerThread_;
+    std::atomic<bool> timerStopFlag_{false};
 };
 
 } // namespace ObjectEditor
