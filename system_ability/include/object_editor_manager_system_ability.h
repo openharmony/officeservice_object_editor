6af2edbe79515036b947bdf1ebf7b4e87ac4b4ef
wei-guoning <weiguoning2@huawei.com>
2026-03-04 14:05:18 +0800

    update
    Signed-off-by: wei-guoning <weiguoning2@huawei.com>

--- system_ability/include/object_editor_manager_system_ability.h
+++ system_ability/include/object_editor_manager_system_ability.h
@@ -20,12 +20,14 @@
 #include <mutex>
 #include "iremote_object.h"
 #include "system_ability.h"
+#include "single_instance.h"
 
-#include "i_object_editor_connection_status_callback.h"
-#include "object_editor_connection.h"
+#include "object_editor_manager_stub.h"
 #include "object_editor_document.h"
 #include "object_editor_format.h"
-#include "object_editor_manager_stub.h"
+#include "object_editor_connection.h"
+#include "object_editor_client_death_recipient.h"
+#include "iobject_editor_connection_status_callback.h"
 
 namespace OHOS {
 namespace ObjectEditor {
@@ -44,17 +46,16 @@ enum class ServiceRunningState {
 class ObjectEditorManagerSystemAbilityConnectionStatusCallback : public IObjectEditorConnectionStatusCallback {
 public:
     ObjectEditorManagerSystemAbilityConnectionStatusCallback() = default;
-    ~ObjectEditorManagerSystemAbilityConnectionStatusCallback() = default;
+    virtual ~ObjectEditorManagerSystemAbilityConnectionStatusCallback() = default;
     void OnConnectionStatusChanged(sptr<IRemoteObject> &remoteObject,
         const ObjectEditorConnectionStatus status) override;
 };
 
 class ObjectEditorManagerSystemAbility : public SystemAbility, public ObjectEditorManagerStub {
     DECLARE_SYSTEM_ABILITY(ObjectEditorManagerSystemAbility);
+    DECLARE_SINGLE_INSTANCE_BASE(ObjectEditorManagerSystemAbility);
 public:
-    DISALLOW_COPY_AND_MOVE(ObjectEditorManagerSystemAbility);
-    ObjectEditorManagerSystemAbility(int32_t saId, bool runOnCreate = true);
-    ObjectEditorManagerSystemAbility() = default;
+    ObjectEditorManagerSystemAbility();
     ~ObjectEditorManagerSystemAbility();
 
     ErrCode StartObjectEditorExtension(std::unique_ptr<ObjectEditorDocument> &document,
@@ -64,37 +65,49 @@ public:
     ErrCode StopObjectEditorExtension(std::unique_ptr<ObjectEditorDocument> &document,
                                       const sptr<IRemoteObject> &oeExtensionRemoteObject,
                                       const bool &isPackageExtension) override;
-    ErrCode GetHmidByFileExtension(const std::string &hmid, std::string &fileExtension) override;
-    ErrCode GetIconByHmid(const std::string &hmid, std::string &resourceId) override;
-    ErrCode GetFormatName(const std::string &hmid, const std::string &locale, std::string &InterName) override;
-    ErrCode GetObjectEditorFormatByHmidAndLocale(const std::string &hmid, const std::string &locale,
-                                                 std::unique_ptr<ObjectEditorFormat> &format) override;
-    ErrCode GetObjectEditorFormatsByLocale(const std::string &locale,
-                                           std::vector<std::unique_ptr<ObjectEditorFormat>> &formats);
+    ErrCode GetHmidByFileExtension(
+        const std::string &hmid,
+        std::string &fileExtension) override;
+    ErrCode GetIconByHmid(
+        const std::string &hmid,
+        std::string &resourceId) override;
+    ErrCode GetFormatName(
+        const std::string &hmid,
+        const std::string &locale,
+        std::string &formatName) override;
+    ErrCode GetObjectEditorFormatByHmidAndLocale(
+        const std::string &hmid,
+        const std::string &locale,
+        std::unique_ptr<ObjectEditorFormat> &format) override;
+    ErrCode GetObjectEditorFormatsByLocale(
+        const std::string &locale,
+        std::vector<std::unique_ptr<ObjectEditorFormat>> &formats) override;
     ErrCode StartUIAbility(const std::unique_ptr<AAFwk::Want> &want) override;
     int32_t CallbackEnter([[maybe_unused]] uint32_t code) override;
     int32_t CallbackExit([[maybe_unused]] uint32_t code, [[maybe_unused]] int32_t result) override;
 
-    static std::mutex connectMapMutex_;
-    static std::map<sptr<IRemoteObject>, sptr<ObjectEditorConnection>> connectMap_;
+    static std::mutex connectionMapMutex_;
+    static std::map<sptr<IRemoteObject>, sptr<ObjectEditorConnection>> connectionMap_;
 
 protected:
     void OnStart() override;
     void OnStop() override;
-    int32_t OnIdle(const SystemAbilityOnDemandReason &infoReason) override;
+    int32_t OnIdle(const SystemAbilityOnDemandReason &idleReason) override;
 
 private:
-    bool ConnectObjectEditorExtAbility(std::unique_ptr<ObjectEditorFormat> &format,
+    bool ConnectObjectEditorExtAbility(
+        std::unique_ptr<ObjectEditorFormat> &format,
         sptr<IRemoteObject> &remoteObject);
+    // 校验客户端APP是否有沙箱临时文件权限
     bool CheckClientFileValid(const ObjectEditorDocument &document);
-    bool GrantClientFilePermissionToServerExtension(const ObjectEditorDocument &document,
+    bool GrantClientFileUriPermissionToServerExtension(const ObjectEditorDocument &document,
         const std::string &targetBundleName);
-    void ReadDiversionJsonFile();
+    void ReadDiversionsJsonFile();
     ObjectEditorManagerErrCode HandlePackage(std::unique_ptr<ObjectEditorDocument> document,
         const sptr<IObjectEditorClientCallback> &clientCallback,
         sptr<IRemoteObject> &oeExtensionRemoteObject, bool &isPackageExtension);
-    ObjectEditorManagerErrCode GetTargetHmid(const std::string &sourceHmid, std::string &targetHmid,
-        std::string &minVersion);
+    ObjectEditorManagerErrCode GetTargetHmid(const std::string &sourceHmid,
+        std::string &targetHmid, std::string &minVersion);
     ObjectEditorManagerErrCode HandleDefaultAppFormatPolicy(const std::string &fileExt,
         std::vector<std::unique_ptr<ObjectEditorFormat>> &formats,
         std::unique_ptr<ObjectEditorFormat> &objectEditorFormat);
